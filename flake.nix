bca301b952c2fbf9e624c4f3320343f99afdbd1d
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2026-03-06 13:09:02 -0500

    bad cpu tyoe

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
             {
               disko.devices.disk.system.device = "/dev/sda";
               networking.hostName = "ham";
-              nixpkgs.hostPlatform = "linux-x86_64";
+              nixpkgs.hostPlatform = "x86_64-linux";
             }
         ];
       };
