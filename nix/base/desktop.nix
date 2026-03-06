157ed7323f224e3bf5bbd8c34a2085f38ad5d064
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2026-03-06 16:15:39 -0500

    Remove home-manager module from desktop.nix imports

--- nix/base/desktop.nix
+++ nix/base/desktop.nix
@@ -1,7 +1,6 @@
 { outputs, inputs, lib, config, pkgs, modulesPath, ... }:{
   imports = [ 
     (modulesPath + "/installer/scan/not-detected.nix")
-    inputs.home-manager.nixosModules.home-manager
     ];
 
 # Security Opts
